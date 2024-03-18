fd42f3b1a868d14406b38b8b94fa6a8214dcd18a
Jilavu Izabela <izabelajilavu@gmail.com>
2024-03-18 15:41:05 +0200

    error fixing

--- GameMap.cpp
+++ GameMap.cpp
@@ -19,18 +19,65 @@ void GameMap::drawMap(sf::RenderWindow& window, float tile_size, Gameplay& gamep
             uint8_t extractedValue = gameplay.extractTileValue(line, column);
             int realValue = Gameplay::create2Power(extractedValue);
 
+            sf::RectangleShape tileShape(sf::Vector2f(tile_size, tile_size));
+            tileShape.setPosition(static_cast<float>(column) * tile_size, static_cast<float>(line) * tile_size);
+
             sf::Text text;
             text.setFont(font);
             text.setString(std::to_string(realValue));
             text.setCharacterSize(50);
             text.setFillColor(sf::Color::Black);
-            text.setPosition(static_cast<float>(column) * tile_size + tile_size/2, static_cast<float>(line) * tile_size + tile_size/2);
+            text.setPosition(
+                    tileShape.getPosition().x + (tile_size - text.getLocalBounds().width) / 2,
+                    tileShape.getPosition().y + (tile_size - text.getLocalBounds().height) / 2
+            );
 
-            sf::RectangleShape tileShape(sf::Vector2f(tile_size, tile_size));
-            tileShape.setPosition(static_cast<float>(column) * tile_size, static_cast<float>(line) * tile_size);
-            tileShape.setFillColor(sf::Color::White);
-            tileShape.setOutlineThickness(5); // Set outline thickness
-            tileShape.setOutlineColor(sf::Color::Black); // Set outline color
+            sf::Color color;
+            switch(realValue) {
+                case 2:
+                    color = sf::Color(243, 230, 250);
+                    break;
+                case 4:
+                    color = sf::Color(239, 209, 255);
+                    break;
+                case 8:
+                    color = sf::Color(231, 192, 252);
+                    break;
+                case 16:
+                    color = sf::Color(222, 164, 252);
+                    break;
+                case 32:
+                    color = sf::Color(216, 146, 252);
+                    break;
+                case 64:
+                    color = sf::Color(210, 126, 252);
+                    break;
+                case 128:
+                    color = sf::Color(203, 104, 252);
+                    break;
+                case 256:
+                    color = sf::Color(155, 62, 201);
+                    break;
+                case 512:
+                    color = sf::Color(100, 28, 135);
+                    break;
+                case 1024:
+                    color = sf::Color(70, 14, 97);
+                    break;
+                case 2048:
+                    color = sf::Color(52, 7, 74);
+                    break;
+                case 4096:
+                    color = sf::Color(36, 5, 51);
+                    break;
+                default:
+                    color = sf::Color::Black;
+            }
+
+            tileShape.setFillColor(color);
+
+//            tileShape.setOutlineThickness(10); // Set outline thickness
+//            tileShape.setOutlineColor(sf::Color::Black); // Set outline color
 
             window.draw(tileShape);
 
